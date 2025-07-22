83d3c393b562f74a75a2c701f3dab4b03581d447
RyanLBenson <S00955017@acad.tri-c.edu>
2025-07-22 02:29:15 +0000

    new file:   Lab1/Lab1.csproj 	modified:   Lab1/Program.cs 	new file:   Lab1/bin/Debug/net8.0/Lab1 	new file:   Lab1/bin/Debug/net8.0/Lab1.deps.json 	new file:   Lab1/bin/Debug/net8.0/Lab1.dll 	new file:   Lab1/bin/Debug/net8.0/Lab1.pdb 	new file:   Lab1/bin/Debug/net8.0/Lab1.runtimeconfig.json 	new file:   Lab1/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs 	new file:   Lab1/obj/Debug/net8.0/Lab1.AssemblyInfoInputs.cache 	new file:   Lab1/obj/Debug/net8.0/Lab1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Lab1/obj/Debug/net8.0/Lab1.GlobalUsings.g.cs 	new file:   Lab1/obj/Debug/net8.0/Lab1.assets.cache 	new file:   Lab1/obj/Debug/net8.0/Lab1.csproj.CoreCompileInputs.cache 	new file:   Lab1/obj/Debug/net8.0/Lab1.csproj.FileListAbsolute.txt 	new file:   Lab1/obj/Debug/net8.0/Lab1.dll 	new file:   Lab1/obj/Debug/net8.0/Lab1.genruntimeconfig.cache 	new file:   Lab1/obj/Debug/net8.0/Lab1.pdb 	new file:   Lab1/obj/Debug/net8.0/Lab1.sourcelink.json 	new file:   Lab1/obj/Debug/net8.0/apphost 	new file:   Lab1/obj/Debug/net8.0/ref/Lab1.dll 	new file:   Lab1/obj/Debug/net8.0/refint/Lab1.dll 	new file:   Lab1/obj/Lab1.csproj.nuget.dgspec.json 	new file:   Lab1/obj/Lab1.csproj.nuget.g.props 	new file:   Lab1/obj/Lab1.csproj.nuget.g.targets 	new file:   Lab1/obj/project.assets.json 	new file:   Lab1/obj/project.nuget.cache 	new file:   ProgrammingLogic.sln

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,72 +1,68 @@
 ﻿using System.Diagnostics.SymbolStore;
 using System.Security.Cryptography;
 
-namespace Lab1;
+Console.WriteLine("Hello, World!");
+//Step 1: Use Comments
+/* Ryan Benson 
+IT-1050 - Lab 1 */
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        //Step 1: Use Comments
-        /* Ryan Benson 
-        IT-1050 - Lab 1 */
+//Step 2: Basic Output
+Console.WriteLine("Ryan Benson");
+Console.WriteLine("IT-1050");
 
-        //Step 2: Basic Output
-        Console.WriteLine("Ryan Benson");
-        Console.WriteLine("IT-1050");
+//Step 3: Declare and Use Variables
+int Num = 87;
+string Language = "I don't have one??";
+double Experience = 0.0D;
+bool myBool = false;
 
-        //Step 3: Declare and Use Variables
-        int Num = 87;
-        string Language = "I don't have one??";
-        double Experience = 0.0D;
-        bool myBool = false;
+Console.WriteLine("My favorite number is " + Num + ", because my favorite car came out that year");
+Console.WriteLine("My favorite programming language is..." + Language);
+Console.WriteLine("The number of programs I've written before this is " + Experience);
+Console.WriteLine("Do I have programming experience? " + myBool);
 
-        Console.WriteLine("My favorite number is " + Num + ", because my favorite car came out that year");
-        Console.WriteLine("My favorite programming language is..." + Language);
-        Console.WriteLine("The number of programs I've written before this is " + Experience);
-        Console.WriteLine("Do I have programming experience? " + myBool);
+//Step 4: Use Constants
+const string school = "Tri-C";
+Console.WriteLine(school);
 
-        //Step 4: Use Constants
-        const string school = "Tri-C";
-        Console.WriteLine(school);
+//Step 5: Type Casting
+double mydouble = 9.78D;
+int myint = (int)mydouble;
+bool mybool = true;
+Console.WriteLine("double: " + mydouble);
+Console.WriteLine("int: " + myint);
+Console.WriteLine(Convert.ToString(myint));
+Console.WriteLine(Convert.ToString(mybool));
 
-        //Step 5: Type Casting
-        double mydouble = 9.78D;
-        int myint = (int)mydouble;
-        bool mybool = true;
-        Console.WriteLine("double: " + mydouble);
-        Console.WriteLine("int: " + myint);
-        Console.WriteLine(Convert.ToString(myint));
-        Console.WriteLine(Convert.ToString(mybool));
+//Step 6: User Input and Type Conversion
+Console.WriteLine("Enter Username: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string username = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+Console.WriteLine("Username is: " + username);
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Welcome " + username + ", You look great for " + age + "!!");
 
-        //Step 6: User Input and Type Conversion
-        Console.WriteLine("Enter Username: ");
-        string username = Console.ReadLine();
-        Console.WriteLine("Username is: " + username);
-        Console.WriteLine("Enter your age:");
-        int age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Welcome " + username + ", You look great for " + age + "!!");
+//Step 7: Arithmetic Operators
+int num1 = 10;
+int num2 = 15;
+Console.WriteLine(num1 + 10);
+Console.WriteLine(num2 + 10);
+Console.WriteLine(num1 - 2);
+Console.WriteLine(num2 - 2);
+Console.WriteLine(num1 * 3);
+Console.WriteLine(num2 * 3);
+Console.WriteLine(num1 / 2);
+Console.WriteLine(num2 / 2);
+Console.WriteLine(num1 % 2);
+Console.WriteLine(num2 % 2);
 
-        //Step 7: Arithmetic Operators
-        int num1 = 10;
-        int num2 = 15;
-        Console.WriteLine(num1 + 10);
-        Console.WriteLine(num2 + 10);
-        Console.WriteLine(num1 - 2);
-        Console.WriteLine(num2 - 2);
-        Console.WriteLine(num1 * 3);
-        Console.WriteLine(num2 * 3);
-        Console.WriteLine(num1 / 2);
-        Console.WriteLine(num2 / 2);
-        Console.WriteLine(num1 % 2);
-        Console.WriteLine(num2 % 2);
-
-        //Step 8: Floating Point Precision
-        float myfloat = 1.123456789F;
-        double myDouble = 1.123456789D;
-        Console.WriteLine(myfloat);
-        Console.WriteLine(myDouble);
+//Step 8: Floating Point Precision
+float myfloat = 1.123456789F;
+double myDouble = 1.123456789D;
+Console.WriteLine(myfloat);
+Console.WriteLine(myDouble);
 
         //Step 9: Increment and Decrement
         int myvar = 10;
@@ -75,16 +71,3 @@ class Program
         myvar--;
         Console.WriteLine(myvar);
         
-
-
-
-
-
-
-
-
-        
-
-
-    }
-}
